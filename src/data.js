210fd4b88429b657a6cb3caeba94a2bfb497172e
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2022-11-01 19:54:31 -0700

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line
+export default [
+  {
+    id: 1,
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+    alt: "Mount Fuji in the distance surrounded by clouds with a treeline along the bottom border.",
+  },
+  {
+    id: 2,
+    title: "Sydney Opera House",
+    location: "Australia",
+    googleMapsUrl: "https://goo.gl/maps/zwGvoqmaogGHvMXk9",
+    startDate: "27 May, 2021",
+    endDate: "8 Jun, 2021",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts center. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+    imageUrl: "https://source.unsplash.com/JmuyB_LibRo",
+    alt: "The Sydney Opera House with the Sydney Harbour Bridge in the background.",
+  },
+  {
+    id: 3,
+    title: "Geirangerfjord",
+    location: "Norway",
+    googleMapsUrl: "https://goo.gl/maps/ynEX1SzFuz7KJ2QD7",
+    startDate: "01 Oct, 2021",
+    endDate: "18 Nov, 2021",
+    description:
+      "The Geirangerfjord is a fjord in the Sonnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality",
+    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
+    alt: "Geirangerfjord with a boat driving up the fjord.",
+  },
+];
